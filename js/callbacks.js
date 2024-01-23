737f74a5b21189a36f2abb236ee8089ab9067ed9
Jack Pope <jack@byramdigitalmedia.com>
2024-01-23 17:47:38 -0500

    create post function

--- js/callbacks.js
+++ js/callbacks.js
@@ -1,16 +1,22 @@
 const posts = [
-  { title: "POST ONE", body: "Hello from post one." },
-  { title: "POST ONE", body: "Hello from post one." }
+  { title: "post one", body: "POST ONE " },
+  { title: "post two", body: "POST TWO" }
 ]
-
 function getPosts() {
   setTimeout(() => {
     let output = ""
     posts.forEach((post, index) => {
-      output += `<li>${post.title}</li> <p>${post.body}</p> <br>`
+      output += `<li>${post.title}</li> <p>${post.body}</p>`
     })
     document.body.innerHTML = output
   }, 1000)
 }
 
-getPosts()
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post)
+    callback()
+  }, 2000)
+}
+
+createPost({ title: "post three", body: "POST THREE" }, getPosts)
