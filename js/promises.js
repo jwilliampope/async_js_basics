cf7c2277b6a331eb0147af31705d7b21151913a0
Jack Pope <jack@byramdigitalmedia.com>
2024-01-24 09:44:02 -0500

    further edits

--- js/promises.js
+++ js/promises.js
@@ -1,47 +1,13 @@
-// const posts = [
-//   { title: "POST ONE", body: "Post One text" },
-//   { title: "POST TWO", body: "Post Two text" }
-// ]
-
-// function getPosts() {
-//   setTimeout(() => {
-//     let output = ""
-//     posts.forEach((post, index) => {
-//       output += `<li>${post.title}</li> <p>${post.body}</p>`
-//     })
-//     document.body.innerHTML = output
-//   }, 1000)
-// }
-
-// function createPost(post) {
-//   return new Promise((res, rej) => {
-//     setTimeout(() => {
-//       posts.push(post)
-//       const error = false
-
-//       if (!error) {
-//         res()
-//       } else {
-//         rej("ERROR")
-//       }
-//     }, 2000)
-//   })
-// }
-
-// createPost({ title: "POST THREE", body: "Post Three text" })
-//   .then(getPosts)
-//   .catch(err => console.log(err))
-
 const posts = [
-  { title: "POST ONE", body: "This is post one." },
-  { title: "POST TWO", body: "This is post two." }
+  { title: "Post One", body: "Post One Text", note: "This is Post One" },
+  { title: "Post One", body: "Post One Text", note: "This is Post Two" }
 ]
 
 function getPosts() {
   setTimeout(() => {
     let output = ""
     posts.forEach((post, index) => {
-      output += `<li>${post.title}</li> <p>${post.body}</p>`
+      output += `<li>${post.title}</li> <p>${post.body}</p> <p>${post.note}</p>`
     })
     document.body.innerHTML = output
   }, 1000)
@@ -51,7 +17,7 @@ function createPost(post) {
   return new Promise((res, rej) => {
     setTimeout(() => {
       posts.push(post)
-      const error = true
+      const error = false
 
       if (!error) {
         res()
@@ -70,6 +36,6 @@ function createPost(post) {
 const promise1 = Promise.resolve("RESOLVED")
 const promise2 = 200
 const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, "COMPLETED"))
-const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json())
+const promise4 = fetch("http://jsonplaceholder.typicode.com/users").then(res => res.json())
 
 Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values))
